db9a387e0ec4253230249e91dd8414a9f58562dd
Philipp Weiss <p.weiss85@gmx.de>
2022-09-29 15:40:51 +0200

    add cart container

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import "./App.css";
+import './App.css';
 
 function App() {
   return (
     <div>
-      <header></header>
+      <header>
+        <h1>React and Morty</h1>
+      </header>
+      <main>
+        <section>
+          <img
+            alt="Character"
+            src="https://rickandmortyapi.com/api/character/avatar/2.jpeg
+"
+          ></img>
+          <h2>Morty Smith</h2>
+          <button>Show more</button>
+        </section>
+      </main>
     </div>
   );
 }
